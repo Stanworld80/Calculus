f411a837f02cce6587799d52921be9ebd6513a20
Stanislas SELLE <stanislas.selle@laposte.fr>
2019-07-29 20:05:44 +0200

    - START : startonresult to go  to RUN and when finnish  comeback on START and then go to SCORE (onActivityResult) as daughter of START
    - correcting some bugs of layout (width and gravity)...

--- app/src/main/java/com/pasqualiselle/calculus/RunActivity.java
+++ app/src/main/java/com/pasqualiselle/calculus/RunActivity.java
@@ -3,23 +3,68 @@ package com.pasqualiselle.calculus;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import static java.lang.Math.random;
 
 public class RunActivity extends AppCompatActivity {
 
+    int mN1 = 0;
+    int mN2 = 0;
+    int mGoodAnswer  = 0;
+    int mLimitMax = 10;
+    EditText answerTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_run);
 
+        answerTextView   = findViewById(R.id.answer_editText);
+
+        prepareQuestion();
         prepareAnswerBtn();
     }
 
+    protected void prepareQuestion() {
+        mN1 = (int) (random() * (mLimitMax+1));
+        mN2 = (int) (random() * (mLimitMax+1));
+        mGoodAnswer = mN1 * mN2;
 
-    protected  void prepareAnswerBtn()
-    {
-       // EditTextView answerTextView =
+        TextView n1view = findViewById(R.id.N1_textView);
+        n1view.setText(String.valueOf(mN1));
+        TextView n2view = findViewById(R.id.N2_textView);
+        n2view.setText(String.valueOf(mN2));
+        answerTextView.setText("");
+
+    }
+
+    protected void prepareAnswerBtn() {
+
+        answerTextView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+
+            @Override
+            public boolean onEditorAction(TextView textView, int actId, KeyEvent keyEvent) {
+                if (actId == EditorInfo.IME_ACTION_DONE) {
+                    String userAnswerStr = textView.getText().toString();
+                    int userAnswer = 0;
+                    if (userAnswerStr.length() > 0)
+                        userAnswer = Integer.parseInt(userAnswerStr);
+
+                    if (mGoodAnswer == userAnswer) {
+                        prepareQuestion();
+                    } else {
+                        Log.d(this.getClass().toString(), "onEditorAction: WRONG ANSWER "+userAnswer + " != " +mGoodAnswer );
+                        finish();
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
     }
 
 }
--- app/src/main/java/com/pasqualiselle/calculus/StartActivity.java
+++ app/src/main/java/com/pasqualiselle/calculus/StartActivity.java
@@ -1,11 +1,14 @@
 package com.pasqualiselle.calculus;
 
 import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 public class StartActivity extends AppCompatActivity {
+
+    private static int RUN_REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +18,16 @@ public class StartActivity extends AppCompatActivity {
 
     public void goRunActivity(View view) {
         Intent intent = new Intent(this, RunActivity.class);
-        startActivity(intent);
+        startActivityForResult(intent,RUN_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RUN_REQUEST_CODE)
+        {
+            Intent intent = new Intent(this, ScoreActivity.class);
+            startActivity(intent);
+        }
     }
 }
