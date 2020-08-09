c2dbb15de311f4c2fc9a428c655e84909033efc1
Stanworld <stanworld@gmail.com>
2020-08-09 19:25:22 +0200

    testing git push

--- app/src/main/java/com/pasqualiselle/calculus/MainActivity.kt
+++ app/src/main/java/com/pasqualiselle/calculus/MainActivity.kt
@@ -1,12 +1,11 @@
 package com.pasqualiselle.calculus
 
-import android.content.Intent
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Toast
-import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
 
