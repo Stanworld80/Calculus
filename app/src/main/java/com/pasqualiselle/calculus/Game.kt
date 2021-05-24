3eeebd83f64e9809d1f00061d634ed152f34daaf
Stanworld <stanworld@gmail.com>
2021-05-24 20:19:21 +0200

    separating a Game Class and binding to it

--- app/src/main/java/com/pasqualiselle/calculus/Game.kt
+++ app/src/main/java/com/pasqualiselle/calculus/Game.kt
@@ -1,16 +1,16 @@
 package com.pasqualiselle.calculus
 
 import android.util.Log
-import androidx.databinding.ObservableInt
-import androidx.navigation.fragment.DialogFragmentNavigatorDestinationBuilder
 
 class Game( ) {
+
     var score: Int = 0
     var currentAnswer: Int? = null
     var currentQuestion: Question = Question()
     val maxAnswer: Int = 999999999
 
-    data class Question(val nb1: Int = (0..9).random(), val nb2: Int = (0..9).random()) {
+    data class Question( val  nb1: Int = (0..9).random(),   val nb2: Int = (0..9).random())
+    {
         val expectedResult = nb1 * nb2
     }
 
